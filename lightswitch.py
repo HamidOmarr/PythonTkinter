28bc44344da7ec778adef73a7a2d327cf140f9b9
HamidOmarr <you@example.com>
2022-03-08 11:39:07 +0100

    'next'

---
+++ lightswitch.py
@@ -0,0 +1,21 @@
+from distutils.cmd import Command
+import tkinter as tk
+from turtle import bgcolor
+
+window = tk.Tk()
+
+def ButtonOn():
+    if(button['text']=='Light Switch Off'):
+        button['text']='Light Switch On'
+        window['background']='yellow'
+
+
+    else:
+        button['text']=('Light Switch Off')
+        window['background']='black'
+        
+button = tk.Button(text='Light Switch Off', background='black', foreground='white',command=ButtonOn)
+button.pack(pady = 20, padx = 20)
+
+
+window.mainloop()
